11aee6f69d49895f4adc49e949e42c4ccc17c4ac
Ermias Asmare <it.ermias.asmare@gmail.com>
2024-08-07 10:20:09 +0300

    updated the .env validation

--- node-api-server/src/config/config.ts
+++ node-api-server/src/config/config.ts
@@ -12,32 +12,12 @@ export const validateEnv = () => {
         return {
             port: +envVars.PORT,
             env: envVars.NODE_ENV,
-            databaseUrl:envVars.DATABASE_URL,
-            smtp: {
-                host: envVars.SMTP_HOST,
-                port: envVars.SMTP_PORT,
-                service: envVars.SMTP_SERVICE,
-                mail: envVars.SMTP_MAIL,
-                password: envVars.SMTP_PASSWORD
-            },
-            jwt: {
-                accessSecret: envVars.JWT_ACCESS_SECRET,
-                refreshSecret: envVars.JWT_REFRESH_SECRET,
-                accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
-                refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
-            },
-            verificationCodeExpire: env.VERIFICATION_CODE_EXP
-            // rateLimiter: {
-            //     maxAttemptsPerDay: envVars.MAX_ATTEMPTS_PER_DAY,
-            //     maxAttemptsByIpUsername: envVars.MAX_ATTEMPTS_BY_IP_USERNAME,
-            //     maxAttemptsPerEmail: envVars.MAX_ATTEMPTS_PER_EMAIL,
-            // },
         };
     } catch (error) {
         if (error instanceof ZodError) {
-            logger.error('Validation failed:', error.errors);
+            console.log('Validation failed:', error.errors);
         } else {
-            logger.error('Error parsing environment variables:', error);
+            console.log('Error parsing environment variables:', error);
         }
     }
 
--- node-api-server/src/validation/env.validation.ts
+++ node-api-server/src/validation/env.validation.ts
@@ -3,21 +3,6 @@ import { z } from 'zod';
 export const envSchema = z.object({
     PORT: z.string({ required_error: "Port number is required" }),
     NODE_ENV: z.enum(['development', "production", "test"]),
-    DATABASE_URL: z.string({ required_error: "Db url is required" }),
-
-    JWT_ACCESS_SECRET: z.string({ required_error: "Access secret is required" }).min(1),
-    JWT_REFRESH_SECRET: z.string({ required_error: "Refresh secret is required" }).min(1),
-    JWT_ACCESS_EXPIRATION_MINUTES: z.string(),
-    JWT_REFRESH_EXPIRATION_DAYS: z.string(),
-
-    VERIFICATION_CODE_EXP: z.string(),
-
-
-    SMTP_HOST: z.string(),
-    SMTP_PORT: z.string(),
-    SMTP_SERVICE: z.string(),
-    SMTP_MAIL: z.string(),
-    SMTP_PASSWORD: z.string()
 });
 
 export type EnvConfig = z.infer<typeof envSchema>;
