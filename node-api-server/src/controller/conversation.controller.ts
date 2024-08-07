796c140348e946bed8d5eb5728c63b609bbcaa25
Ermias Asmare <it.ermias.asmare@gmail.com>
2024-08-07 11:06:02 +0300

    add the get history by userId and get conversation by id function to conversation controller and service files

--- node-api-server/src/controller/conversation.controller.ts
+++ node-api-server/src/controller/conversation.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { selectModels, sendQuerys } from '../services/conversation.services';
+import { getConversationById, getConversationHistory, selectModels, sendQuerys } from '../services/conversation.services';
 
 export const selectModel = async (req: Request, res: Response) => {
     const { userId, modelName } = req.body;
@@ -22,5 +22,25 @@ export const sendQuery = async (req: Request, res: Response) => {
         res.status(200).json(response.response);
     } catch (error) {
         res.status(500).json({ error: 'Error sending query to model' });
+    }
+};
+
+export const getHistory = async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    try {
+        const history = await getConversationHistory(userId)
+        res.status(200).json(history);
+    } catch (error) {
+        res.status(500).json({ error: 'Error getting conversation history' });
+    }
+};
+
+export const getConversation = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const conversation = await getConversationById(id)
+        res.status(200).json(conversation);
+    } catch (error) {
+        res.status(500).json({ error: 'Error getting conversation' });
     }
 };
--- node-api-server/src/services/conversation.services.ts
+++ node-api-server/src/services/conversation.services.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import conversationModel from "../models/conversation.model";
+import NotFoundError from "../error/notFound.errors";
+import { ErrorCode } from "../error/custom.errors";
 
 export const selectModels = async (userId: string, modelName: string) => {
     try {
@@ -23,4 +25,19 @@ export const sendQuerys = async(userId: string, query:string) => {
     await newConversation.save();
 
     return newConversation
+}
+
+export const getConversationHistory = async(userId: string) => {
+    const conversations = await conversationModel.find({ userId }).sort({ createdAt: -1 });
+    if (!conversations) throw new NotFoundError("conversation not found", ErrorCode.ITEM_NOT_FOUND)
+
+    return conversations
+}
+
+
+export const getConversationById = async(id: string) => {
+    const conversation = await conversationModel.findById(id);
+    if (!conversation) throw new NotFoundError("conversation not found", ErrorCode.ITEM_NOT_FOUND)
+
+    return conversation
 }
